afb8432daca0da7a227e08ea70d31f4e9569adf2
DGorgula <gil@dankner.org>
2021-09-27 11:44:21 +0300

    dotenv load fixed
    fooling around with allegrograph
    
    Signed-off-by: DGorgula <gil@dankner.org>

--- allegrographServer.py
+++ allegrographServer.py
@@ -1,9 +1,30 @@
+#   Dotenv
 from dotenv import dotenv_values
 config = dotenv_values(".env")
+locals().update(config)
 
+#   RDF
 from franz.openrdf.connect import ag_connect
-with ag_connect('test', host=config["HOST"], port=config["PORT"],
-                user=config["USER"], password=config["PASS"]) as conn:
-    print (conn.size())
+from franz.openrdf.vocabulary import RDF
 
-# server = AllegroGraphServer(host="localhost", port=10035, user="danks", password="4ll3gr0p4ss")
+with ag_connect('test', host=HOST, port=PORT, user=USER, password=PASS, clear=True) as conn:
+    # print (conn.size())
+    person = conn.createURI("http://example.org/ontology/Person")
+    name = conn.createURI("http://example.org/ontology/name")
+    bob = conn.createURI("http://example.org/people/bob")
+    bobsName = conn.createLiteral("Bob")
+    alice = conn.createURI("http://example.org/people/alice")
+    alicesName = conn.createLiteral("Alice")
+    conn.add(bob, RDF.TYPE, person)
+    conn.add(bob, name, bobsName)
+    conn.add(alice, RDF.TYPE, person)
+    conn.add(alice, name, alicesName)
+
+    print (conn.size())
+    for statement in conn.getStatements():
+        print(statement)
+    # conn.add(alice, RDFFormat, person)
+    # conn.add(alice, name, alicesName)
+    # conn.add(bob, RDFFormat, person)
+    # conn.add(bob, name, bobsName)
+    # print (conn.size())
