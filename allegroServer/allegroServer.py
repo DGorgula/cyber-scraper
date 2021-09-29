4fb1628c321d426de0856f815e464fab098896a6
DGorgula <gil@dankner.org>
2021-09-30 01:49:56 +0300

    routes added
    get '/post/all' route added
    
    post '/post/new' route added
    
    '/author' route changed to '/author/new'
    
    Signed-off-by: DGorgula <gil@dankner.org>

--- allegroServer/allegroServer.py
+++ allegroServer/allegroServer.py
@@ -13,12 +13,12 @@ from franz.openrdf.vocabulary import RDF
 conn = ag_connect('test', host=HOST, port=PORT, user=USER, password=PASS, create=True, clear=True)
 basePath = "ex://"
 conn.setNamespace('ex', basePath)
-Post = conn.createURI("http://example.org/ontology/Post")
+post = conn.createURI("http://example.org/ontology/Post")
 title = conn.createURI("http://example.org/ontology/title")
 name = conn.createURI("http://example.org/ontology/name")
 content = conn.createURI("http://example.org/ontology/content")
 publishDate = conn.createURI("http://example.org/ontology/publish-date")
-postLink = conn.createURI("http://example.org/ontology/postlink")
+link = conn.createURI("http://example.org/ontology/postlink")
 image = conn.createURI("http://example.org/ontology/image")
 author = conn.createURI("http://example.org/ontology/Author")
 about = conn.createURI("http://example.org/ontology/about")
@@ -33,7 +33,7 @@ def hello(name = "Danks-param"):
     paramName= name
     return f'Hello, {escape(queryName)}, {escape(paramName)}!'
 
-@app.route('/author', methods=['post'])
+@app.route('/author/new', methods=['post'])
 def postAuthor():
 
     authorName = request.get_json()["name"]
@@ -63,8 +63,45 @@ def getAllAuthors():
             authorsData[nextIndex][p[4]] = data[2].replace("\'","").replace('\"',"")
     print(dir(authors))
     print(str(authorsData))
-    return str(authorsData)
+    return {"allAuthors": authorsData}
 
+@app.route('/post/new', methods=['post'])
+def postPost():
+
+    postTitle = request.get_json()["title"]
+    postLink = request.get_json()["link"]
+    postImage = request.get_json()["image"]
+    postContent = request.get_json()["content"]
+    postPublishDate = request.get_json()["publishDate"]
+    postAuthor = conn.createURI(f'http://example.org/author/{request.get_json()["author"].replace(" ", "-")}')
+    newPost = conn.createURI(f'http://example.org/post/{postTitle.replace(" ", "-")}')
+    print(postTitle, postLink, postImage, postContent, postPublishDate, postAuthor)
+    conn.add(newPost, RDF.TYPE, post)
+    conn.add(newPost, title, postTitle)
+    conn.add(newPost, link, postLink)
+    conn.add(newPost, image, postImage)
+    conn.add(newPost, content, postContent)
+    conn.add(newPost, publishDate, postPublishDate)
+    conn.add(newPost, author, postAuthor)
+    return "done"
+
+@app.route('/post/all')
+def getAllPosts():
+    postsData = []
+    posts = conn.getStatements(None, None, post).string_tuples
+    for a in posts:
+        print("a", a[0])
+        nextIndex = len(postsData)
+        authorRawData = conn.getStatements(a[0], None, None).string_tuples
+        postsData.append({})
+        for data in authorRawData:
+            print("data", data)
+            p = data[1].replace(">","").split("/")
+            if len(p) > 5 : continue
+            postsData[nextIndex][p[4]] = data[2].replace("\'","").replace('\"',"")
+    print(dir(posts))
+    print(str(postsData))
+    return {"allposts": postsData}
 
 if __name__ == '__main__':
     app.run(debug=True)
